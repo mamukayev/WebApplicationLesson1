71efc250483055e894521f3639956f94237fedc2
Sasha <sasha03.smit@gmail.com>
2024-04-05 17:43:50 +0300

    происходит сложение переданных в последних двух сегментах чисел и возвращается строка

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            switch (c)
+            {
+                case null:
+                case "+": return $"{a} + {b} = {a + b}";
+                case "-": return $"{a} - {b} = {a - b}";
+                case "*": return $"{a} * {b} = {a * b}";
+                default:
+                    return "Только операции +,-,*";
+            }
+
+        }
+    }
+}
