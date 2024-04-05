2951c258aadee432f4d3aefb2cdbf57ae7d5f19f
Sasha <sasha03.smit@gmail.com>
2024-04-05 17:26:22 +0300

    калькулятор, который выполняет сложение переданных в последних двух сегментах чисел и возвращается строка

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b)
+        {
+            return $"{a} + {b} = {a + b}";
+             
+        }
+    }
+}
