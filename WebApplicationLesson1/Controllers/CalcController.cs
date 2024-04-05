88b62e933282856156aafabfe543383b2c910882
Sasha <sasha03.smit@gmail.com>
2024-04-05 18:01:13 +0300

    происходит сложение значений переданных параметров a и b и возвращается строка

---
+++ WebApplicationLesson1/Controllers/CalcController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            switch (c)
+            {
+                case null:
+                case "+": return $"{a} + {b} = {a + b}";
+                case "-": return $"{a} - {b} = {a - b}";
+                case "*": return $"{a} * {b} = {a * b}";
+                case "/": return b != 0 ? $"{a} / {b} = {a / b}" : "На ноль делить нельзя";
+                default:
+                    return "Только операции +,-,*";
+            }
+
+        }
+    }
+}
